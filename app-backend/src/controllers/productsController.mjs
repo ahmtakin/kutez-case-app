b787d9e925772d5da8a22b13dedd7475141486fd
ahmtakin <aa.akin57@gmail.com>
2024-12-08 18:25:34 +0300

    Add slider and fonts

--- app-backend/src/controllers/productsController.mjs
+++ app-backend/src/controllers/productsController.mjs
@@ -11,27 +11,23 @@ const __dirname = path.dirname(__filename);
 dotenv.config({ path: path.resolve(__dirname, '../../.env') });
 const GOLD_API_URL = process.env.GOLD_API_URL; 
 const GOLD_API_KEY = process.env.GOLD_API_KEY; 
-//const GOLD_API_URL = "https://api.metalpriceapi.com/v1/latest";
-//const GOLD_API_KEY = "ebb94d9f1856b29c816bbd96cce44d16";
+
  
 const getGoldPrice = async () => {
-  console.log(`${GOLD_API_URL}?api_key=${GOLD_API_KEY}&base=USD&currencies=XAU`);
+  // console.log(`${GOLD_API_URL}?api_key=${GOLD_API_KEY}&base=USD&currencies=XAU`);
   try {
-    const response = await axios.get(GOLD_API_URL, {
-      params: {
-        api_key: GOLD_API_KEY,
-        base: 'USD',
-        currencies: 'XAU', // Gold price symbol
-      },
-    });
-    //console.log('Final URL:', `${METALS_API_URL}?${new URLSearchParams(params)}`);
+    const pricePerOunce=2632.664; // The api was working but I have used up all my free usage so this is gold price atm.
+  //   const response = await axios.get(GOLD_API_URL, {
+  //     params: {
+  //       api_key: GOLD_API_KEY,
+  //       base: 'USD',
+  //       currencies: 'XAU', // Gold price symbol
+  //     },
+  //   });
     // Gold price is often given per ounce; convert to grams
-    console.log("here1")
-    const pricePerOunce = response.data.rates.USDXAU;
-    console.log("hello there",pricePerOunce);
+    //const pricePerOunce = response.data.rates.USDXAU;
     //const pricePerGram= pricePerOunce;
     const pricePerGram = pricePerOunce / 28.35; // 1 ounce = 28.35 grams
-    console.log("pricepergr",pricePerGram);
     return pricePerGram;
   } catch (error) {
     console.error('Error fetching gold price:', error.message);
@@ -50,15 +46,12 @@ export const getProducts = async (req, res) => {
     const filePath = path.resolve('./data/products.json');
     const fileData = await fs.readFile(filePath, 'utf-8');
     const products = JSON.parse(fileData);
-    console.log("file initialized")
     const goldPrice = await getGoldPrice();
-    console.log("retrieved gold prices")
     // Calculate prices for each product
     const productsWithPrices = products.map((product) => ({
       ...product,
       price: calculateProductPrice(product.popularityScore, product.weight, goldPrice).toFixed(2), // Price formatted to 2 decimal places
     }));
-    console.log("parsed all")
     res.json(productsWithPrices);
   } catch (err) {
     res.status(500).json({ error: 'Failed to load products' });
