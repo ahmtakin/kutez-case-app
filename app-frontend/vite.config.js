844548cc48912285ba20662f247aecf2a03eec50
ahmtakin <aa.akin57@gmail.com>
2024-12-08 19:41:54 +0300

    Config environment

--- app-frontend/vite.config.js
+++ app-frontend/vite.config.js
@@ -1,22 +1,22 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
+import dotenv from 'dotenv'
+
+dotenv.config();
+const API_ENDPOINT=process.env.API_ENDPOINT;
 
 export default defineConfig({
   plugins: [react()],
   server: {
     proxy: {
       '/api': {
-        target: 'http://localhost:4000', // Backend server
+        target: API_ENDPOINT, // Backend server
         changeOrigin: true,
         secure: false,
       },
     },
   },
-  // resolve: {
-  //   alias: {
-  //     swiper: 'swiper/swiper-bundle.esm.js', // Explicitly resolve to ES module version of Swiper
-  //   },
-  // },
+
 });
 
 
